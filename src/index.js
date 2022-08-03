ca8d3903ceb5ea90e2566a5c049771f9981b5cbb
who0803 <10rlatkdtn@naver.com>
2022-08-03 16:33:17 +0900

    feat: Add current score
    플레이어의 현재 점수 추가

--- src/index.js
+++ src/index.js
@@ -6,20 +6,47 @@ let turnFlag = true;  // true : fisrtPlayuer, false : secondPlayer
 
 let rollBtn = document.querySelector('.rollBtn');
 let bankBtn = document.querySelector('.bankBtn');
+let firstPlayerScore = document.querySelector('.firstPlayerScore');
+let secondPlayerScore = document.querySelector('.secondPlayerScore');
+let firstPlayerCurrScore = document.querySelector('.firstPlayerCurrScore');
+let secondPlayerCurrScore = document.querySelector('.secondPlayerCurrScore');
+firstPlayerScore.innerHTML = 0;
+secondPlayerScore.innerHTML = 0;
+firstPlayerCurrScore.innerHTML = 0;
+secondPlayerCurrScore.innerHTML = 0;
 
 rollBtn.addEventListener('click', ()=>{
+  // 랜덤 넘버 생성
+  // 점수 누적
+
   diceNumber = Math.floor(Math.random() * (6-1)+1);  // 1~6 랜덤 넘버
+  sum += diceNumber;
   if (diceNumber == 1) {
     sum = 0;
+    turnFlag = !turnFlag; // 턴바꿈
   } 
-  sum += diceNumber;
+
+  if (turnFlag) {
+    firstPlayerCurrScore.innerHTML = sum;
+  }
+  else {
+    secondPlayerCurrScore.innerHTML += sum;
+  }
+  
 })
 
 bankBtn.addEventListener('click', () => {
+  // 눌렀을 경우 누적 점수 반영
+  // 턴 바뀜
   if (turnFlag) {
     firstPlayer += sum;
+    firstPlayerScore.innerHTML = firstPlayer;
   }
   else {
     secondPlayer += sum;
+    secondPlayerScore.innerHTML = secondPlayer;
   }
+  sum = 0;
+  flag = !flag; // 턴바꿈
 })
+
