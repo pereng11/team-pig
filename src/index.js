5dac9fffda6c305c46da2b3816847f3f86edbc05
who0803 <10rlatkdtn@naver.com>
2022-08-03 16:15:22 +0900

    feat: Add function to reset sum if dice number is one
    1이 나왔을 경우 점수 리셋

--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ let sum = 0;
 let rollBtn = document.querySelector('.rollBtn');
 rollBtn.addEventListener('click', ()=>{
   diceNumber = Math.floor(Math.random() * (6-1)+1);  // 1~6 랜덤 넘버
+  if (diceNumber == 1) {
+    sum = 0;
+  } 
   sum += diceNumber;
 })
