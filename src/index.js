cb7162cbc6ec02c477971b52a32e76d71ba77923
who0803 <10rlatkdtn@naver.com>
2022-08-03 16:12:57 +0900

    feat: Add roll button to create random number
    눌렀을 경우 랜덤 넘버가 나오는 버튼을 생성하고 점수 누적

---
+++ src/index.js
@@ -0,0 +1,8 @@
+let diceNumber = 0;
+let sum = 0;
+
+let rollBtn = document.querySelector('.rollBtn');
+rollBtn.addEventListener('click', ()=>{
+  diceNumber = Math.floor(Math.random() * (6-1)+1);  // 1~6 랜덤 넘버
+  sum += diceNumber;
+})
