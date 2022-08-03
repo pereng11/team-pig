244211b15a9bfc7710ac0c83b42c0f40381a5f48
who0803 <10rlatkdtn@naver.com>
2022-08-03 16:22:12 +0900

    feat: Add event to accumulate scores ,when click bank button
    뱅크 버튼 클릭 시 플레이어 점수 누적

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 let diceNumber = 0;
 let sum = 0;
+let firstPlayer = 0;
+let secondPlayer = 0;
+let turnFlag = true;  // true : fisrtPlayuer, false : secondPlayer
 
 let rollBtn = document.querySelector('.rollBtn');
+let bankBtn = document.querySelector('.bankBtn');
+
 rollBtn.addEventListener('click', ()=>{
   diceNumber = Math.floor(Math.random() * (6-1)+1);  // 1~6 랜덤 넘버
   if (diceNumber == 1) {
     sum = 0;
   } 
   sum += diceNumber;
+})
+
+bankBtn.addEventListener('click', () => {
+  if (turnFlag) {
+    firstPlayer += sum;
+  }
+  else {
+    secondPlayer += sum;
+  }
 })
